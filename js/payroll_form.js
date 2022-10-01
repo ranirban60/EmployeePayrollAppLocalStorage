75551e7750f87a9c141c73c28b0e0a12b68d5ab2
ranirban60 <ranirban60@gmail.com>
2022-10-01 11:47:58 +0530

    Added: Set event listener on salary range and validate name and date

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,16 +1,22 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-salary.addEventListener('input', function () {
-  output.textContent = salary.value;
-});
+window.addEventListener('DOMContentLoaded', (event) => {
+  const name = document.querySelector('#name');
+  const textError = document.querySelector('.name-error');
+  name.addEventListener('input',function() {
+      if(name.value.length == 0){
+          textError.textContent = "";
+          return;
+      }
+      try {
+          (new EmployeePayrollData()).name = name.value;
+          textError.textContent = "";
+      }catch (e) {
+          textError.textContent = e;
+      }
+  });
 
-const text = document.querySelector('#name');
-const nameError = document.querySelector('.name-error');
-text.addEventListener('input', function () {
-  let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
-  if (nameRegex.test(text.value)) {
-    nameError.textContent = '';
-    let name = text.value;
-  }
-  else nameError.textContent = 'Name is Incorrect';
-});
+  const salary = document.querySelector('#salary');
+  const output = document.querySelector('.salary-output');
+  salary.addEventListener('input',function(){
+      output.textContent=salary.value;
+  });
+})
